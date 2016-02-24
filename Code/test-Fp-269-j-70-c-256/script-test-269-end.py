ee031e9eddc2ce1ad05fc6ab2219768309d5d117
Hugounenq-Cyril <hugounenq@msn.com>
2016-02-24 15:58:07 +0100

    script test endo FP269

--- Code/test-Fp-269-j-70-c-256/script-test-269-end.py
+++ Code/test-Fp-269-j-70-c-256/script-test-269-end.py
@@ -1,8 +1,9 @@
 load('/data/cyril/github/Code/isogeny-computing-testless.sage')
 load('/data/cyril/github/Code/tate-module-extension-testless.sage')
 load('/data/cyril/github/Code/extension_corps.sage')
-
-
+#load('~/Documents/github-3/Code/isogeny-computing-testless.sage')
+#load('~/Documents/github-3/Code/tate-module-extension-testless.sage')
+#load('~/Documents/github-3/Code/extension_corps.sage')
 
 F=FiniteField(269)
 K=Tower_two(F,1)
@@ -11,9 +12,15 @@ E=EllipticCurve(j=F(70))
 if E.cardinality()!=256:
 	E=E.quadratic_twist()
 print E
-deg=end
-a,b,c,d,e,f,g=tate_module(E,((16.0)/3)*deg,K,2,conservation=True)
-#a,b2,c2,d,e,f,g=tate_module(E3,((16.0)/3)*deg,K,2,conservation=True)
-R=PolynomialRing(b[0].parent(),'x')
-P2,Q2,Tower,L,M,Lc,Vr,o2,o1=calcul_isogenie_initialisation(b,c,b,c,R,2,d,d,deg,e,f,g)
-
+for end in range(2,24):
+	deg=end**2
+	a,b,c,d,e,f,g=tate_module(E,((16.0)/3)*deg,K,2,conservation=True)
+	a,b2,c2,d,e,f,g=tate_module(E,((16.0)/3)*deg,K,2,conservation=True)
+	R=PolynomialRing(b[0].parent(),'x')
+	P2,Q2,Tower,L,M,Lc,Vr,o2,o1=calcul_isogenie_initialisation(b,c,b2,c2,R,2,d,d,deg,e,f,g)
+	A=timeit('calcul_isogenie_etape(P2,Q2,Tower,L,M,Lc,Vr,o2,o1,2)',number=10,repeat=5,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=10,repeat=5,seconds=True,preparse=True)
+	C=timeit('calcul_isogenie_initialisation(b,c,b2,c2,R,2,d,d,deg,e,f,g)',number=10,repeat=5,seconds=True,preparse=True)
+	Fichier = open('/data/cyril/github/Code/test-script-269.txt','a')
+	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
+	Fichier.close()
