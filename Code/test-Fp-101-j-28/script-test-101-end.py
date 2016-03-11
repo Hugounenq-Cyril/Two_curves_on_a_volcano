28c1868dc8758080945bd8e450f6c9d435dfea1e
Hugounenq-Cyril <hugounenq@msn.com>
2016-03-11 14:42:45 +0100

    script test

--- Code/test-Fp-101-j-28/script-test-101-end.py
+++ Code/test-Fp-101-j-28/script-test-101-end.py
@@ -5,11 +5,11 @@ load('/data/cyril/github/Code/extension_corps.sage')
 #load('~/Documents/github-3/Code/tate-module-extension-testless.sage')
 #load('~/Documents/github-3/Code/extension_corps.sage')
 
-F=FiniteField(269)
+F=FiniteField(101)
 K=Tower_two(F,1)
 print K._top
-E=EllipticCurve(j=F(70))
-if E.cardinality()!=256:
+E=EllipticCurve(j=F(28))
+if E.cardinality()!=96:
 	E=E.quadratic_twist()
 print E
 for end in range(2,24):
@@ -21,7 +21,7 @@ for end in range(2,24):
 	A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,d)',number=10,repeat=5,seconds=True,preparse=True)
 	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=10,repeat=5,seconds=True,preparse=True)
 	C=timeit('calcul_isogenie_init(b,c,b2,c2,R,2,d,d,deg,e,f,g)',number=10,repeat=5,seconds=True,preparse=True)
-	Fichier = open('/data/cyril/github/Code/test-script-269.txt','a')
-	#Fichier=open('~/Documents/github-3/Code/test-script-269.txt','a')
+	Fichier = open('/data/cyril/github/Code/test-script-101.txt','a')
+	#Fichier=open('~/Documents/github-3/Code/test-script-101.txt','a')
 	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
 	Fichier.close()
