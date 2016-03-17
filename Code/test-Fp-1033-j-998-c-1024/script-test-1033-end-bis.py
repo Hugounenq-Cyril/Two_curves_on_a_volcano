6bd08715bfa1f5382cf1fdd97a3c3f5200d3e34b
Hugounenq-Cyril <hugounenq@msn.com>
2016-03-17 18:06:22 +0100

    tests mieux echnatillones

--- Code/test-Fp-1033-j-998-c-1024/script-test-1033-end-bis.py
+++ Code/test-Fp-1033-j-998-c-1024/script-test-1033-end-bis.py
@@ -21,12 +21,12 @@ for end in range(7,13):
 	else:
 		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)
 	if calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)[0]==True:
-		A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=10,repeat=5,seconds=True,preparse=True)
+		A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=20,repeat=5,seconds=True,preparse=True)
 	else:
-		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=10,repeat=5,seconds=True,preparse=True)
-	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=10,repeat=5,seconds=True,preparse=True)
-	C=timeit('calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)',number=10,repeat=5,seconds=True,preparse=True)
-	Fichier = open('/data/cyril/github/Code/test-script-1033-bis.txt','a')
+		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=20,repeat=5,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=20,repeat=5,seconds=True,preparse=True)
+	C=timeit('calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)',number=20,repeat=5,seconds=True,preparse=True)
+	Fichier = open('/data/cyril/github/Code/test-script-1033-bis2.txt','a')
 	#Fichier=open('~/Documents/github-3/Code/test-script-1033.txt','a')
 	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
 	Fichier.close()
