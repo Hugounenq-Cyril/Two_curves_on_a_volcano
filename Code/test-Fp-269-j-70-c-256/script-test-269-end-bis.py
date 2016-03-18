dbaa794ffc0612756dcf87b657c4b198dba1b324
Hugounenq-Cyril <hugounenq@msn.com>
2016-03-18 09:38:50 +0100

    nouveaux tests

--- Code/test-Fp-269-j-70-c-256/script-test-269-end-bis.py
+++ Code/test-Fp-269-j-70-c-256/script-test-269-end-bis.py
@@ -16,18 +16,18 @@ for end in range(7,13):
 	deg=end**2
 	a,b,c,d,e,f,g=tate_module(E,((16.0)/3)*deg,K,2,conservation=True)
 	a,b2,c2,d,e,f,g=tate_module(E,((16.0)/3)*deg,K,2,conservation=True)
-	if calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)[0]==True:
-		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init_bis(b,c,b2,c2,2,d,deg,e,f,g)
-		C=timeit('calcul_isogenie_init_bis(b,c,b2,c2,2,d,deg,e,f,g)',number=20,repeat=5,seconds=True,preparse=True)
-		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=20,repeat=5,seconds=True,preparse=True)		
+	if calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)[0]==True:
+		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init_bis(b,c,b,c,2,d,deg,e,f,g)
+		C=timeit('calcul_isogenie_init_bis(b,c,b,c,2,d,deg,e,f,g)',number=10,repeat=10,seconds=True,preparse=True)
+		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=10,repeat=10,seconds=True,preparse=True)		
 	else:
-		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)
-		C=timeit('calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)',number=20,repeat=5,seconds=True,preparse=True)		
+		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)
+		C=timeit('calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)',number=10,repeat=10,seconds=True,preparse=True)		
 		if calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)[0]==True:
-			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=20,repeat=5,seconds=True,preparse=True)
+			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=10,repeat=10,seconds=True,preparse=True)
 		else:
-			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=20,repeat=5,seconds=True,preparse=True)
-	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=20,repeat=5,seconds=True,preparse=True)
+			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=10,repeat=10,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=10,repeat=10,seconds=True,preparse=True)
 	Fichier = open('/data/cyril/github/Code/test-script-269-bis2.txt','a')
 	#Fichier=open('~/Documents/github-3/Code/test-script-269.txt','a')
 	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
