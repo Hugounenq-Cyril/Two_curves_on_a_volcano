7e3138368efe2df4295d8098af059335a8270f6b
Hugounenq-Cyril <hugounenq@msn.com>
2016-03-21 10:57:36 +0100

    second oubli

--- Code/test-Fp-149-j-64-c-160/script-test-149-end.py
+++ Code/test-Fp-149-j-64-c-160/script-test-149-end.py
@@ -22,7 +22,7 @@ for end in range(2,56):
 		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)		
 	else:
 		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)
-		C=timeit('calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)',number=10,repeat=15,seconds=True,preparse=True)		
+		C=timeit('calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)',number=15,repeat=10,seconds=True,preparse=True)		
 		if calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)[0]==True:
 			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
 		else:
@@ -45,7 +45,7 @@ for l in L:
 		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)		
 	else:
 		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)
-		C=timeit('calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)',number=10,repeat=15,seconds=True,preparse=True)		
+		C=timeit('calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)',number=15,repeat=10,seconds=True,preparse=True)		
 		if calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)[0]==True:
 			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
 		else:
