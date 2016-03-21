d8e3ca710f150a3e7e4de1026be5ca1c2ec7b0ac
Hugounenq-Cyril <hugounenq@msn.com>
2016-03-21 10:51:57 +0100

    oubli modif precedente

--- Code/test-Fp-149-j-64-c-160/script-test-149-end.py
+++ Code/test-Fp-149-j-64-c-160/script-test-149-end.py
@@ -27,7 +27,7 @@ for end in range(2,56):
 			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
 		else:
 			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
-	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=10,repeat=5,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=15,repeat=10,seconds=True,preparse=True)
 	Fichier = open('/data/cyril/github/Code/test-script-149-ter.txt','a')
 	#Fichier=open('~/Documents/github-3/Code/test-script-149.txt','a')
 	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
@@ -50,7 +50,7 @@ for l in L:
 			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
 		else:
 			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
-	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=10,repeat=5,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=15,repeat=10,seconds=True,preparse=True)
 	Fichier = open('/data/cyril/github/Code/test-script-149-ter.txt','a')
 	#Fichier=open('~/Documents/github-3/Code/test-script-149.txt','a')
 	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
