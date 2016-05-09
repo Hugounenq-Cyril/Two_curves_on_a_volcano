d51a646592600864c438b30517e8ada4678bf5d2
Hugounenq-Cyril <hugounenq@msn.com>
2016-05-09 15:12:17 +0200

    test pour benchmark3

---
+++ Code/test-Fp-101-j-28-c-96/script-test-101-end-ter.py
@@ -0,0 +1,75 @@
+load('../isogeny-computing-testless.sage')
+load('../tate-module-extension-testless.sage')
+load('../extension_corps.sage')
+
+F=FiniteField(101)
+K=Tower_two(F,1)
+print K._top
+E=EllipticCurve(j=F(28))
+if E.cardinality()!=96:
+	E=E.quadratic_twist()
+print E
+for end in range(2,443):
+	deg=end**2
+	a,b,c,d,e,f,g=tate_module(E,((16.0)/3)*deg,K,2,conservation=True)
+	a,b2,c2,d,e,f,g=tate_module(E,((16.0)/3)*deg,K,2,conservation=True)
+	if calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)[0]==True:
+		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init_bis(b,c,b,c,2,d,deg,e,f,g)
+		C=timeit('calcul_isogenie_init_bis(b,c,b,c,2,d,deg,e,f,g)',number=3,repeat=5,seconds=True,preparse=True)
+		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=3,repeat=5,seconds=True,preparse=True)		
+	else:
+		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)
+		C=timeit('calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)',number=3,repeat=5,seconds=True,preparse=True)		
+		if calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)[0]==True:
+			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=3,repeat=5,seconds=True,preparse=True)
+		else:
+			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=3,repeat=5,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=3,repeat=5,seconds=True,preparse=True)
+	Fichier = open('../../benchmarks/test-script-101-quater.tsv','a')
+	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
+	Fichier.close()
+L=[23,59]
+for l in L:
+	deg=l
+	a,b,c,d,e,f,g=tate_module(E,((16.0)/3)*deg,K,2,conservation=True)
+	a,b2,c2,d,e,f,g=tate_module(E,((16.0)/3)*deg,K,2,conservation=True)
+	if calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)[0]==True:
+		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init_bis(b,c,b,c,2,d,deg,e,f,g)
+		C=timeit('calcul_isogenie_init_bis(b,c,b,c,2,d,deg,e,f,g)',number=3,repeat=5,seconds=True,preparse=True)
+		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=3,repeat=5,seconds=True,preparse=True)		
+	else:
+		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)
+		C=timeit('calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)',number=3,repeat=5,seconds=True,preparse=True)		
+		if calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)[0]==True:
+			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=3,repeat=5,seconds=True,preparse=True)
+		else:
+			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=3,repeat=5,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=3,repeat=5,seconds=True,preparse=True)
+	Fichier = open('../../benchmarks/test-script-101-quater.tsv','a')
+	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
+	Fichier.close()
+
+
+L=[3,13,29,31,41,47,71,73]
+E3=EllipticCurve(j=F(65))
+if E3.cardinality()!=96:
+	E3=E3.quadratic_twist()
+for l in L:
+	deg=l
+	a,b,c,d,e,f,g=tate_module(E,((16.0)/3)*deg,K,2,conservation=True)
+	a,b2,c2,d,e,f,g=tate_module(E3,((16.0)/3)*deg,K,2,conservation=True)
+	if calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)[0]==True:
+		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init_bis(b,c,b2,c2,2,d,deg,e,f,g)
+		C=timeit('calcul_isogenie_init_bis(b,c,b2,c2,2,d,deg,e,f,g)',number=3,repeat=5,seconds=True,preparse=True)
+		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=3,repeat=5,seconds=True,preparse=True)		
+	else:
+		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)
+		C=timeit('calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)',number=3,repeat=5,seconds=True,preparse=True)		
+		if calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)[0]==True:
+			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=3,repeat=5,seconds=True,preparse=True)
+		else:
+			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=3,repeat=5,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=3,repeat=5,seconds=True,preparse=True)
+	Fichier = open('../../benchmarks/test-script-101-quater.tsv','a')
+	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
+	Fichier.close()
