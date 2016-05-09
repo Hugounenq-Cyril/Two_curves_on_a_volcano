6a138976bcaa844e6acb39c9471e90fccda7f1f6
Hugounenq-Cyril <hugounenq@msn.com>
2016-05-09 15:43:19 +0200

    test pour benchmark2 correctif

--- Code/test-Fp-101-j-28-c-96/script-test-101-end-bis.py
+++ Code/test-Fp-101-j-28-c-96/script-test-101-end-bis.py
@@ -24,7 +24,7 @@ for end in range(2,443):
 			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=1,repeat=5,seconds=True,preparse=True)
 		else:
 			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=1,repeat=5,seconds=True,preparse=True)
-	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=15,repeat=10,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=1,repeat=5,seconds=True,preparse=True)
 	Fichier = open('../../benchmarks/test-script-101-ter.tsv','a')
 	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
 	Fichier.close()
