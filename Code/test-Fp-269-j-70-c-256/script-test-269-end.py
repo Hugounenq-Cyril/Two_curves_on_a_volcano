69377884d40415fa80ab510d20e578f472ddc020
Hugounenq-Cyril <hugounenq@msn.com>
2016-05-10 23:35:35 +0200

    nettoyage code

--- Code/test-Fp-269-j-70-c-256/script-test-269-end.py
+++ Code/test-Fp-269-j-70-c-256/script-test-269-end.py
@@ -2,6 +2,11 @@ load('../isogeny-computing-testless.sage')
 load('../tate-module-extension-testless.sage')
 load('../extension_corps.sage')
 
+n=15
+r=10
+Fichier = open('../../benchmarks/test-script-269-bis.tsv','a')
+
+
 F=FiniteField(269)
 K=Tower_two(F,1)
 print K._top
@@ -15,17 +20,16 @@ for end in range(2,443):
 	a,b2,c2,d,e,f,g=tate_module(E,((16.0)/3)*deg,K,2,conservation=True)
 	if calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)[0]==True:
 		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init_bis(b,c,b,c,2,d,deg,e,f,g)
-		C=timeit('calcul_isogenie_init_bis(b,c,b,c,2,d,deg,e,f,g)',number=15,repeat=10,seconds=True,preparse=True)
-		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)		
+		C=timeit('calcul_isogenie_init_bis(b,c,b,c,2,d,deg,e,f,g)',number=n,repeat=r,seconds=True,preparse=True)
+		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=n,repeat=r,seconds=True,preparse=True)		
 	else:
 		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)
-		C=timeit('calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)',number=15,repeat=10,seconds=True,preparse=True)		
+		C=timeit('calcul_isogenie_init(b,c,b,c,2,d,deg,e,f,g)',number=n,repeat=r,seconds=True,preparse=True)		
 		if calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)[0]==True:
-			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
+			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=n,repeat=r,seconds=True,preparse=True)
 		else:
-			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
-	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=15,repeat=10,seconds=True,preparse=True)
-	Fichier = open('../../benchmarks/test-script-269-bis.tsv','a')
+			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=n,repeat=r,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=n,repeat=r,seconds=True,preparse=True)
 	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
 	Fichier.close()
 
@@ -40,17 +44,16 @@ for l in L:
 	a,b2,c2,d,e,f,g=tate_module(E3,((16.0)/3)*deg,K,2,conservation=True)
 	if calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)[0]==True:
 		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init_bis(b,c,b2,c2,2,d,deg,e,f,g)
-		C=timeit('calcul_isogenie_init_bis(b,c,b2,c2,2,d,deg,e,f,g)',number=15,repeat=10,seconds=True,preparse=True)
-		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)		
+		C=timeit('calcul_isogenie_init_bis(b,c,b2,c2,2,d,deg,e,f,g)',number=n,repeat=r,seconds=True,preparse=True)
+		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=n,repeat=r,seconds=True,preparse=True)		
 	else:
 		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)
-		C=timeit('calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)',number=15,repeat=10,seconds=True,preparse=True)		
+		C=timeit('calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)',number=n,repeat=r,seconds=True,preparse=True)		
 		if calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)[0]==True:
-			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
+			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=n,repeat=r,seconds=True,preparse=True)
 		else:
-			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
-	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=15,repeat=10,seconds=True,preparse=True)
-	Fichier = open('../../benchmarks/test-script-269-bis.tsv','a')
+			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=n,repeat=r,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=n,repeat=r,seconds=True,preparse=True)
 	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
 	Fichier.close()
 E3=EllipticCurve(j=F(195))
@@ -64,17 +67,16 @@ for l in L:
 	a,b2,c2,d,e,f,g=tate_module(E3,((16.0)/3)*deg,K,2,conservation=True)
 	if calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)[0]==True:
 		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init_bis(b,c,b2,c2,2,d,deg,e,f,g)
-		C=timeit('calcul_isogenie_init_bis(b,c,b2,c2,2,d,deg,e,f,g)',number=15,repeat=10,seconds=True,preparse=True)
-		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)		
+		C=timeit('calcul_isogenie_init_bis(b,c,b2,c2,2,d,deg,e,f,g)',number=n,repeat=r,seconds=True,preparse=True)
+		A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=n,repeat=r,seconds=True,preparse=True)		
 	else:
 		M,Lc,P2,Q2,R2,o2,o1,Tower,deg=calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)
-		C=timeit('calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)',number=15,repeat=10,seconds=True,preparse=True)		
+		C=timeit('calcul_isogenie_init(b,c,b2,c2,2,d,deg,e,f,g)',number=n,repeat=r,seconds=True,preparse=True)		
 		if calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)[0]==True:
-			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
+			A=timeit('calcul_isogenie_step_bis(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=n,repeat=r,seconds=True,preparse=True)
 		else:
-			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=15,repeat=10,seconds=True,preparse=True)
-	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=15,repeat=10,seconds=True,preparse=True)
-	Fichier = open('../../benchmarks/test-script-269-bis.tsv','a')
+			A=timeit('calcul_isogenie_step(M,Lc,P2,Q2,R2,o2,o1,Tower,deg)',number=n,repeat=r,seconds=True,preparse=True)
+	B=timeit('tate_module(E,((16.0)/3)*deg,K,2,conservation=True)',number=n,repeat=r,seconds=True,preparse=True)
 	Fichier.write( str(deg) +'\t'+ str(B) +'\t'+ str(C) +'\t'+ str(A) + '\t'+ str(d) +'\n')
 	Fichier.close()
 
